b3a63677bbb017be334c1a8c3229c35e3e5df40a
Jon Sporring <sporring@di.ku.dk>
2016-07-25 17:52:23 +0000

    indentation

---
+++ src/identifiersExampleAdvance.fsx
@@ -0,0 +1,13 @@
+let solution a b c sgn =
+  let determinant a b c =
+    b ** 2.0 - 2.0 * a * c
+  let d = determinant a b c
+  (-b + sgn * sqrt d) / (2.0 * a)
+
+let a = 1.0
+let b = 0.0
+let c = -1.0
+let xp = solution a b c +1.0
+let xn = solution a b c -1.0
+printfn "%A * x ** 2.0 + %A * x + %A" a b c
+printfn "  has solutions %A and %A" xn xp
