d70c611cb790e23722cb19440f5a42e91d53141d
Jon Sporring <sporring@di.ku.dk>
2016-07-28 09:37:53 +0000

    comment part

--- src/commentExample.fsx
+++ src/commentExample.fsx
@@ -1,18 +1,31 @@
+/// Calculate the determinant of a quadratic equation with parameters a, b, and c
+let determinant a b c =
+  b ** 2.0 - 2.0 * a * c
+
 /// <summary>Find x when 0 = ax^2+bx+c.</summary>
+/// <remarks>Negative determinants are not checked.</remarks>
+/// <example>
+///   The following code:
+///   <code>
+///     let a = 1.0
+///     let b = 0.0
+///     let c = -1.0
+///     let xp = (solution a b c +1.0)
+///     printfn "0 = %.1fx^2 + %.1fx + %.1f => x_+ = %.1f" a b c xp
+///   </code>
+///   results in <c>0 = 1.0x^2 + 0.0x + -1.0 => x_+ = 0.7</c> printed to the console.
+/// </example>
 /// <param name="a">Quadratic coefficient.</param>
 /// <param name="b">Linear coefficient.</param>
 /// <param name="c">Constant coefficient.</param>
 /// <param name="sgn">+1 or -1 indicating which solution is to be calculated.</param>
-/// <returns>x.</returns>
+/// <returns>The solution to x.</returns>
 let solution a b c sgn =
-  /// Calculate the determinant of a quadratic equation with parameters a, b, and c
-  let determinant a b c =
-    b ** 2.0 - 2.0 * a * c
   let d = determinant a b c
   (-b + sgn * sqrt d) / (2.0 * a)
 
 let a = 1.0
 let b = 0.0
 let c = -1.0
-let xp = solution a b c +1.0
-printfn "%.1fx^2 + %.1fx+%.1f=0 => x=%.1f" a b c xp
+let xp = (solution a b c +1.0)
+printfn "0 = %.1fx^2 + %.1fx + %.1f => x_+ = %.1f" a b c xp
