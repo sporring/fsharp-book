47dc33e2b0931146b772232879b0678eeeaf50d4
Jon Sporring <sporring@di.ku.dk>
2016-08-05 09:18:09 +0000

    test chapter done

---
+++ src/date2DayBlackTest.fsx
@@ -0,0 +1,56 @@
+let januaryFirstDay (y : int) =
+  let a = (y - 1) % 4
+  let b = (y - 1) % 100
+  let c = (y - 1) % 400
+  (1 + 5 * a + 4 * b + 6 * c) % 7
+
+let rec sum (lst : int list) j =
+  if 0 <= j && j < lst.Length then
+    lst.[0] + sum lst.[1..] (j - 1)
+  else
+    0
+  
+let date2Day d m y =
+  let dayPrefix = ["Sun"; "Mon"; "Tues"; "Wednes"; "Thurs"; "Fri"; "Satur"]
+  let feb = if (y % 4 = 0) && ((y % 100 <> 0) || (y % 400 = 0)) then 29 else 28
+  let daysInMonth = [31; feb; 31; 30; 31; 30; 31; 31; 30; 31; 30; 31]
+  let dayOne = januaryFirstDay y
+  let daysSince = (sum daysInMonth (m - 2)) + d - 1
+  let weekday = (dayOne + daysSince) % 7;
+  dayPrefix.[weekday] + "day"
+
+let testCases = [
+  ("A complete week",
+   [(1, 1, 2016, "Friday");
+    (2, 1, 2016, "Saturday");
+    (3, 1, 2016, "Sunday");
+    (4, 1, 2016, "Monday");
+    (5, 1, 2016, "Tuesday");
+    (6, 1, 2016, "Wednesday");
+    (7, 1, 2016, "Thursday");]);
+  ("Across boundaries",
+   [(31, 12, 2014, "Wednesday");
+    (1, 1, 2015, "Thursday");
+    (30, 9, 2017, "Saturday");
+    (1, 10, 2017, "Sunday")]);
+  ("Across feburary bondary",
+   [(28, 2, 2016, "Sunday");
+    (29, 2, 2016, "Monday");
+    (1, 3, 2016, "Tuesday");
+    (28, 2, 2017, "Tuesday");
+    (1, 3, 2017, "Wednesday")]);
+  ("Leap years",
+   [(1, 3, 2015, "Sunday");
+    (1, 3, 2012, "Thursday");
+    (1, 3, 2000, "Wednesday");
+    (1, 3, 2100, "Monday")]);
+  ]
+
+printfn "Black-box testing of date2Day.fsx"
+for i = 0 to testCases.Length - 1 do
+  let (setName, testSet) = testCases.[i]
+  printfn "  %d. %s" (i+1) setName
+  for j = 0 to testSet.Length - 1 do
+    let (d, m, y, expected) = testSet.[j]
+    let day = date2Day d m y
+    printfn "    test %d - %b" (j+1) (day = expected)
