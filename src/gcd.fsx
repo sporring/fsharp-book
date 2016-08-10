b77573bab34cdb443c113ba442af4c25a8151447
Jon Sporring <sporring@di.ku.dk>
2016-08-10 14:04:07 +0000

    hand tracing done

---
+++ src/gcd.fsx
@@ -0,0 +1,12 @@
+let rec gcd a b = |\label{gcd:gcd}|
+  if a < b then |\label{gcd:if}|
+    gcd b a |\label{gcd:gcdR1}|
+  elif b > 0 then |\label{gcd:elif}|
+    gcd b (a % b)  |\label{gcd:gcdR2}|
+  else 
+    a |\label{gcd:return}|
+
+let a = 10 |\label{gcd:a}|
+let b = 15 |\label{gcd:b}|
+printfn "gcd %d %d = %d" a b (gcd a b) |\label{gcd:printfn}|
+
--- src/lexicalScopeNFunction.fsx
+++ src/lexicalScopeNFunction.fsx
@@ -1,6 +1,6 @@
 let testScope x =
   let a = 3.0
-  let f z = a * x
+  let f z = a * z
   let a = 4.0
   f x
 printfn "%A" (testScope 2.0)
