ffd66f6e8e3ed37bd90c80032cecfa0e6d624b9b
Jon Sporring <sporring@di.ku.dk>
2016-08-13 11:53:11 +0000

    working on io

---
+++ src/userDialogue.fsx
@@ -0,0 +1,6 @@
+System.Console.WriteLine "To perform the multiplication of a and b"
+System.Console.Write "Enter a: "
+let a = float (System.Console.ReadLine ())
+System.Console.Write "Enter b: "
+let b = float (System.Console.ReadLine ())
+System.Console.WriteLine ("a * b = " + string (a * b))
