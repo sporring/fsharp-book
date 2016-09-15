10cc4c089f3d86f920c230852e5ae858643d81b1
Jon Sporring <sporring@di.ku.dk>
2016-09-15 21:21:46 +0000

    Chapters 6 and 7 ready for publication

--- src/commentExample.fsx
+++ src/commentExample.fsx
@@ -1,9 +1,8 @@
-/// Calculate the determinant of a quadratic equation with parameters a, b, and c
-let determinant a b c =
-  b ** 2.0 - 2.0 * a * c
+/// The discriminant of a quadratic equation with parameters a, b, and c
+let discriminant a b c = b ** 2.0 - 2.0 * a * c
 
 /// <summary>Find x when 0 = ax^2+bx+c.</summary>
-/// <remarks>Negative determinants are not checked.</remarks>
+/// <remarks>Negative discriminant are not checked.</remarks>
 /// <example>
 ///   The following code:
 ///   <code>
@@ -13,15 +12,15 @@ let determinant a b c =
 ///     let xp = (solution a b c +1.0)
 ///     printfn "0 = %.1fx^2 + %.1fx + %.1f => x_+ = %.1f" a b c xp
 ///   </code>
-///   results in <c>0 = 1.0x^2 + 0.0x + -1.0 => x_+ = 0.7</c> printed to the console.
+///   prints <c>0 = 1.0x^2 + 0.0x + -1.0 => x_+ = 0.7</c> to the console.
 /// </example>
 /// <param name="a">Quadratic coefficient.</param>
 /// <param name="b">Linear coefficient.</param>
 /// <param name="c">Constant coefficient.</param>
-/// <param name="sgn">+1 or -1 indicating which solution is to be calculated.</param>
+/// <param name="sgn">+1 or -1 determines the solution.</param>
 /// <returns>The solution to x.</returns>
 let solution a b c sgn =
-  let d = determinant a b c
+  let d = discriminant a b c
   (-b + sgn * sqrt d) / (2.0 * a)
 
 let a = 1.0
