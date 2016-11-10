96c052c09633be4d9930426a313132fc7c163a4f
Jon Sporring <sporring@di.ku.dk>
2016-11-10 14:04:24 +0000

    minor

--- src/winforms/Window.fsx
+++ src/winforms/Window.fsx
@@ -1,32 +1,17 @@
 ﻿/// Open a window using winforms in Mono. The program opens a window
 /// and draws a black triangle inside it.
-///
-/// How to compile:
-/// <code>
-/// fsharpc Window.fsx
-/// </code>
-///
-/// Author: Jon Sporring.
-/// Date: 2015/11/19
-
 open System.Drawing
 open System.Windows.Forms
 
-/// Set parameters for drawing.
+// Choose some points and a color
+let Points = [|Point (0,0); Point (10,170); Point (320,20); Point (0,0)|]
 let penColor = Color.Black
-let listOfPoints = [Point (0,0); Point (10,170); Point (320,20); Point (0,0)]
 
-/// Convert list of points to an array, create a pen, and create the
-/// panel data structure and add its draw function.
-let pArr = Array.ofList listOfPoints
+// Setup drawing function
 let pen = new Pen (penColor)
-let panel = new Panel (Dock = DockStyle.Fill)
-panel.Paint.Add (fun e -> e.Graphics.DrawLines (pen, pArr))
-
-/// Create the window data structure and add its panel.
 let win = new Form ()
-win.Controls.Add panel
+win.Paint.Add (fun e -> e.Graphics.DrawLines (pen, Points))
 
-/// Create the window and turn over control to the operating
-/// system. Use "win.Show()" on Microsoft Windows instead.
+// Create the window and turn over control to the operating
+// system. Use "win.Show()" on Microsoft Windows instead.
 Application.Run win
--- src/winforms/Window2.fsx
+++ src/winforms/Window2.fsx
@@ -1,28 +1,17 @@
 ﻿/// Open a window using winforms in Mono. The program opens a window
 /// and draws a black triangle inside it. Wrap the display part in a function.
-///
-/// How to compile:
-/// <code>
-/// fsharpc Window.fsx
-/// </code>
-///
-/// Author: Jon Sporring.
-/// Date: 2015/11/19
 
 open System.Drawing
 open System.Windows.Forms
 
-let display(title: string, lst:(float*float) list, color:Color) =
+let display (title: string) (lst:(float*float) []) (color:Color) =
     let f(x,y) = Point (int (round x), int(round y))
-    let pArr = Array.ofList (List.map f lst)
+    let pArr = Array.map f lst
     let pen = new Pen (color)
     let draw (g:Graphics) = g.DrawLines (pen, pArr)
-    let panel = new Panel (Dock = DockStyle.Fill)
     let win = new Form (Text = title)
-
-    panel.Paint.Add (fun e -> draw (e.Graphics))
-    win.Controls.Add panel
+    win.Paint.Add (fun e -> draw (e.Graphics))
     Application.Run win
 
-let listOfPoints = [(0.0,0.0); (10.0,170.0); (320.0,20.0); (0.0,0.0)]
-display("My second window", listOfPoints, Color.Black)
+let listOfPoints = [|(0.0,0.0); (10.0,170.0); (320.0,20.0); (0.0,0.0)|]
+display "My second window", listOfPoints, Color.Black
