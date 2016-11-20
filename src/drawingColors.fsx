5c2ec9c48b4640ce4c77fe6055982e8cdfd4b6ab
Jon Sporring <sporring@di.ku.dk>
2016-11-20 22:56:28 +0000

    chapter 13 published

--- src/drawingColors.fsx
+++ src/drawingColors.fsx
@@ -4,6 +4,8 @@ open System.Drawing
 let c = Color.FromArgb (0xFF, 0x7F, 0xFF, 0xD4) //Aquamarine
 printfn "The color  %A is (%x, %x, %x, %x)" c c.A c.R c.G c.B
 // Define a list of named colors
-let colors = [Color.Red; Color.Green; Color.Blue; Color.Black; Color.Gray; Color.White]
+let colors =
+  [Color.Red; Color.Green; Color.Blue;
+   Color.Black; Color.Gray; Color.White]
 for col in colors do
   printfn "The color %A is (%x, %x, %x, %x)" col col.A col.R col.G col.B
