ce2dcb705a3353b1a9e1896cd0ec9b89748ca01b
Jon Sporring <sporring@di.ku.dk>
2022-09-20 08:52:19 +0200

    minor

--- src/fibRecursiveAlt.fsx
+++ src/fibRecursiveAlt.fsx
@@ -3,6 +3,7 @@ let fib (n: uint) =
     if n < 2u then pair
     else fibPair (n - 1u) (snd pair, fst pair + snd pair)
 
-  fibPair n (0u, 1u) |> snd
+  if n < 2u then n
+  else fibPair n (0u, 1u) |> snd
 
 printfn "fib(10) = %A" (fib 10u)
